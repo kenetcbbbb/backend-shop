a86102723e226366f275eccaa79bfcc6851fce2a
Анастасия Максимович <anastasiamaksimovich@MacBook-Air-Anastasia.local>
2026-01-31 07:11:28 +0300

    Connect to working Render database

--- src/config/db.js
+++ src/config/db.js
@@ -5,14 +5,12 @@ dotenv.config();
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-// Проверка подключения
 pool.connect()
   .then(() => console.log("PostgreSQL connected ✅"))
   .catch((err) => console.error("PostgreSQL connection error", err));
